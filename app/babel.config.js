6992d30675d83d4e5dafd94a7f7e361f57fcfb28
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-06-14 16:32:18 -0300

    Configure root import

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,14 +1,14 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'babel-plugin-root-import',
-        {
-          rootPathSuffix: 'src',
-        },
-      ],
-    ],
-  };
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'babel-plugin-root-import',
+				{
+					rootPathSuffix: 'src',
+				},
+			],
+		],
+	};
 };
